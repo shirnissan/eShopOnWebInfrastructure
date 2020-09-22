df67aef063bc4fa4ea1fe1f90fb6cb14c04b5b77
shirnissan <59134238+shirnissan@users.noreply.github.com>
2020-09-22 17:22:27 +0300

    Update eShop.tf

--- eShop.tf
+++ eShop.tf
@@ -43,18 +43,9 @@ data "aws_ami" "aws-linux" {
   owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["amzn-ami-hvm*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name   = "image-id"
+    values = ["ami-0c94855ba95c71c99"]
+  
   }
 }
 
