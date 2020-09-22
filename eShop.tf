1400ff2c7cebab83949f3567e564eae411800753
shirnissan <59134238+shirnissan@users.noreply.github.com>
2020-09-22 14:54:21 +0300

    Update eShop.tf

--- eShop.tf
+++ eShop.tf
@@ -6,6 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
+variable "jenkins_url" {}
 variable "region" {
   default = "us-east-1"
 }
