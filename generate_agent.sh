6dd7606e7b49da57595bd64054c43376a5b593a2
shirnissan <59134238+shirnissan@users.noreply.github.com>
2020-09-22 14:46:50 +0300

    Update generate_agent.sh

--- generate_agent.sh
+++ generate_agent.sh
@@ -6,10 +6,10 @@ NODE_NAME=`hostname`
 NUM_EXECUTORS=1
 
 # Download CLI jar from the master
-curl ${MASTER_URL}:8080/jnlpJars/jenkins-cli.jar -o ~/jenkins-cli.jar
+curl ${JENKINS_URL}/jnlpJars/jenkins-cli.jar -o ~/jenkins-cli.jar
 
 # Create node according to parameters passed in
-cat <<EOF | java -jar ~/jenkins-cli.jar -auth "${MASTER_USERNAME}:${MASTER_PASSWORD}" -s "${MASTER_URL}:8080" create-node "${NODE_NAME}" |true
+cat <<EOF | java -jar ~/jenkins-cli.jar -auth "${MASTER_USERNAME}:${MASTER_PASSWORD}" -s "${JENKINS_URL}" create-node "${NODE_NAME}" |true
 <slave>
   <name>${NODE_NAME}</name>
   <description></description>
@@ -35,8 +35,8 @@ EOF
 
 
 # Download slave.jar
-curl ${MASTER_URL}:8080/jnlpJars/slave.jar -o /usr/share/jenkins/slave.jar
+curl ${JENKINS_URL}/jnlpJars/slave.jar -o /usr/share/jenkins/slave.jar
 
 # Run jnlp launcher
-java -jar /usr/share/jenkins/slave.jar -jnlpUrl ${MASTER_URL}:8080/computer/${NODE_NAME}/slave-agent.jnlp -jnlpCredentials "${MASTER_USERNAME}:${MASTER_PASSWORD}"
+java -jar /usr/share/jenkins/slave.jar -jnlpUrl ${JENKINS_URL}/computer/${NODE_NAME}/slave-agent.jnlp -jnlpCredentials "${MASTER_USERNAME}:${MASTER_PASSWORD}"
 
