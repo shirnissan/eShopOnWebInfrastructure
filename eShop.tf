496bbfbb3c71a42b4f0695cdea9e77c41d2f420c
shirnissan <59134238+shirnissan@users.noreply.github.com>
2020-09-30 21:50:02 +0300

    Update eShop.tf

--- eShop.tf
+++ eShop.tf
@@ -107,6 +107,13 @@ resource "aws_security_group" "elb-sg" {
   #Allow HTTP from anywhere
   ingress {
     from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
